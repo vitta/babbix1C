3d39ec145756950ef2bbf181541bb821b1edbc91
Mikhail Baranov <mikhail.baranov@gmail.com>
2012-10-04 13:39:23 +0400

    Изменение состояния страницы в момент загрузки
    Никаких чисел и строк. Только плейсхолдеры

--- routes/index.js
+++ routes/index.js
@@ -7,19 +7,7 @@ var http = require('http');
 exports.index = function (req, res) {
     res.render('index', {
         title:'Babbix',
-        time: new Date().getHours() + ":" + new Date().getMinutes(),
-        counts: {
-            triggers: {
-                disaster: 1,
-                high: 4,
-                average: 1
-            },
-            tasks: {
-                expired: 2,
-                new: 4,
-                total: 43
-            }
-        }
+        time: new Date().getHours() + ":" + new Date().getMinutes()
     });
 };
 
